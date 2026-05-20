b35c607bdf7247f9771e7022100895619ba42abc
Alessandro Cacciatore <38426513+geronimaw@users.noreply.github.com>
2026-05-20 22:52:05 +0200

    Add all possible args configurations

--- src/main_train.sh
+++ src/main_train.sh
@@ -1,4 +1,8 @@
-DYLD_LIBRARY_PATH=$HOME/homebrew/opt/libomp/lib python3 ./main_train.py \
-                                                        --model_type svm \
-                                                        --multi_dataset \
-                                                        --train_on_X_first_freq 58
+python3 ./main_train.py --model_type svm 
+python3 ./main_train.py --model_type svm --train_on_X_first_freq 58
+python3 ./main_train.py --model_type svm --multi_dataset
+python3 ./main_train.py --model_type svm --multi_dataset --train_on_X_first_freq 58
+python3 ./main_train.py --model_type xgb 
+python3 ./main_train.py --model_type xgb --train_on_X_first_freq 58
+python3 ./main_train.py --model_type xgb --multi_dataset
+python3 ./main_train.py --model_type xgb --multi_dataset --train_on_X_first_freq 58
